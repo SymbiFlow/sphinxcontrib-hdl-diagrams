00294b768a638ff26cd062853bef8f79b9d8edf5
Robert Winkler <rwinkler@antmicro.com>
2020-09-14 15:45:10 +0200

    Apply review comments
    Signed-off-by: Robert Winkler <rwinkler@antmicro.com>

--- compat/sphinxcontrib_verilog_diagrams/__init__.py
+++ compat/sphinxcontrib_verilog_diagrams/__init__.py
@@ -1,11 +1,11 @@
+import warnings
 import sphinxcontrib_hdl_diagrams
 
 def setup(app):
-    print("")
-    print("WARNING:")
-    print("  sphinxcontrib-verilog-diagram extension is depreciated!")
-    print("  Please use sphinxcontrib-hdl-diagrams instead:")
-    print("  https://github.com/SymbiFlow/sphinxcontrib-hdl-diagrams")
-    print("")
+    deprecation_msg = """
+    sphinxcontrib-verilog-diagram extension is depreciated!
+    Please use sphinxcontrib-hdl-diagrams instead:
+    https://github.com/SymbiFlow/sphinxcontrib-hdl-diagrams"""
+    warnings.warn(deprecation_msg, DeprecationWarning)
 
     return sphinxcontrib_hdl_diagrams.setup(app)
