1dd6eb53a8b4680446cce0a401ade91d1fed81bf
Daniel Lim Wee Soong <weesoong.lim@gmail.com>
2021-04-15 10:53:20 +0800

    Update licenses headers.
    Signed-off-by: Daniel Lim Wee Soong <weesoong.lim@gmail.com>
    Signed-off-by: Tim 'mithro' Ansell <tansell@google.com>

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2020  The SymbiFlow Authors.
+# Copyright (C) 2020-2021  The SymbiFlow Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
